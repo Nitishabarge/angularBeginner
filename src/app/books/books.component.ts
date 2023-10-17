641a294c9d49ad443158c9b320077377d618c72f
Nitisha Patil <nitishapatil@Gauravs-MacBook-Pro.local>
2023-10-17 13:06:18 -0400

    ngModel,twoway binding,property binding

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent {
+name ="Fourth Wing";
+author="Rebecca Yarros";
+src="https://m.media-amazon.com/images/I/81im9aAFBOL._SL1500_.jpg";
+name2 ="No Brainer";
+author2="Jeff Kinney";
+src2="https://m.media-amazon.com/images/I/71pL+3nMzfL._SL1475_.jpg";
+name3 ="Spooky Pookie";
+author3="Sandra Boynton";
+src3="https://m.media-amazon.com/images/I/618INpEnVjL._SL1500_.jpg";
+isDisabled=false;
+textname="";
+}
