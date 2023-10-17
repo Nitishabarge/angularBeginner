1254a1ebe083cfc4ef4e3f76932cec82dc643600
Nitisha Patil <nitishapatil@Gauravs-MacBook-Pro.local>
2023-10-17 13:23:25 -0400

    ngFor directive

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 
+interface book {
+name:string,
+author:string,
+src:string
+
+}
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
-name ="Fourth Wing";
-author="Rebecca Yarros";
-src="https://m.media-amazon.com/images/I/81im9aAFBOL._SL1500_.jpg";
-name2 ="No Brainer";
-author2="Jeff Kinney";
-src2="https://m.media-amazon.com/images/I/71pL+3nMzfL._SL1475_.jpg";
-name3 ="Spooky Pookie";
-author3="Sandra Boynton";
-src3="https://m.media-amazon.com/images/I/618INpEnVjL._SL1500_.jpg";
-isDisabled=false;
-textname="";
+
+   books : book[] = [{
+    name :"Fourth Wing",
+    author:"Rebecca Yarros",
+    src:"https://m.media-amazon.com/images/I/81im9aAFBOL._SL1500_.jpg",
+  },
+  {
+    name :"No Brainer",
+    author:"Jeff Kinney",
+    src:"https://m.media-amazon.com/images/I/71pL+3nMzfL._SL1475_.jpg",
+  },
+  {
+    name :"Spooky Pookie",
+    author:"Sandra Boynton",
+    src:"https://m.media-amazon.com/images/I/618INpEnVjL._SL1500_.jpg",
+  }
+
+  ]
+
+
+// isDisabled=false;
+// textname="";
 }
