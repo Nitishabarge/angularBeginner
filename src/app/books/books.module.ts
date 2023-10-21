621ea9ca505a00f652c10c3bdec8ac5373f4180e
Nitisha Patil <nitishapatil@Gauravs-MacBook-Pro.local>
2023-10-21 14:56:38 -0400

    Routing setup

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books.component';
-// import { FormsModule } from '@angular/forms';
 import { ChildBooksComponent } from './child-books/child-books.component';
-import { BooksServiceService } from './books-service.service';
 
 
 @NgModule({
