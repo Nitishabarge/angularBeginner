7a7a1fd1c76bb2fdd0365ae5de09ef74364496c8
Nitisha Patil <nitishapatil@Gauravs-MacBook-Pro.local>
2023-10-23 14:29:35 -0400

    register form design

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form: any = {
+    email: "",
+    password: "",
+    cn_password:""
 
+  }
 }
